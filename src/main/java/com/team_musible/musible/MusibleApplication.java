50bcba8dde9e0bb8fec8aa76dcb98e2b97a81b29
PPPosik <eoanswkgksk@gmail.com>
2021-04-11 21:47:32 +0900

    initial commit

---
+++ src/main/java/com/team_musible/musible/MusibleApplication.java
@@ -0,0 +1,13 @@
+package com.team_musible.musible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team_musible/musible/MusibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_musible.musible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
