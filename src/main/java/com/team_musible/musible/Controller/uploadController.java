307b520d5ab2993786088b68f10eca082de0127b
Reno <reno.urssu@gmail.com>
2021-05-09 20:10:15 +0900

    파일 업로드 기능 구현

---
+++ src/main/java/com/team_musible/musible/Controller/uploadController.java
@@ -0,0 +1,29 @@
+package com.team_musible.musible.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+@EnableWebMvc
+@RestController
+public class uploadController {
+    @PostMapping("/upload")
+    @ResponseStatus(HttpStatus.CREATED)
+    public List<String> upload(@RequestPart List<MultipartFile> files) throws Exception {
+        List<String> list = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String originalfileName = file.getOriginalFilename();
+            File dest = new File("F:/MusibleImage/" + originalfileName);
+            file.transferTo(dest);
+        }
+        return list;
+    }
+}
