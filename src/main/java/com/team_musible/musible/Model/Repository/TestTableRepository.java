ce94bf500a02385b186d1bafeff3b6377812e9b7
NotableDeveloper <castlereal@naver.com>
2021-05-15 17:31:53 +0900

    누락된 파일 추가

---
+++ src/main/java/com/team_musible/musible/Model/Repository/TestTableRepository.java
@@ -0,0 +1,7 @@
+package com.team_musible.musible.Model.Repository;
+
+import com.team_musible.musible.Model.Entity.TestTable;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestTableRepository extends CrudRepository<TestTable, Long> {
+}
