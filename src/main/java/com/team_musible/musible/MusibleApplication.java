e38a807cdea373ee9a876b7c69b0ab55b037dea9
Reno <61468564+NotableDeveloper@users.noreply.github.com>
2021-05-15 17:09:40 +0900

    Revert "Feat: file upload"

--- src/main/java/com/team_musible/musible/MusibleApplication.java
+++ src/main/java/com/team_musible/musible/MusibleApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusibleApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(MusibleApplication.class, args);
 	}
+
 }
